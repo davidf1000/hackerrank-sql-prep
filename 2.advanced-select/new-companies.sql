827ad712cc642315afcc30ceafa9c8d6dbdebe8a
davidf1000 <davidfauzi29@gmail.com>
2022-07-16 11:26:00 +0700

    add file for every question

--- 2.advanced-select/new-companies.sql
+++ 2.advanced-select/new-companies.sql
@@ -1,9 +1,8 @@
-select c.company_code, c.founder, 
-    count(distinct l.lead_manager_code), count(distinct s.senior_manager_code), 
-    count(distinct m.manager_code),count(distinct e.employee_code) 
-from Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e 
-where c.company_code = l.company_code 
-    and l.lead_manager_code=s.lead_manager_code 
-    and s.senior_manager_code=m.senior_manager_code 
-    and m.manager_code=e.manager_code 
-group by c.company_code order by c.company_code;
+SELECT c.company_code, c.founder, COUNT(DISTINCT l.lead_manager_code), COUNT(DISTINCT s.senior_manager_code), COUNT(DISTINCT m.manager_code), COUNT(DISTINCT e.employee_code)
+FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e
+WHERE c.company_code = l.company_code
+     AND l.company_code = s.company_code
+     AND s.company_code = m.company_code
+     AND m.company_code = e.company_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
